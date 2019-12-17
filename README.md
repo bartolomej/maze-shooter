34bba9a42134e8f2ca64a7f3652f96e7217a05cb
bartolomej <bartolomej.kozorog@gmail.com>
2019-12-17 19:40:58 +0100

    Initialized codebase with webpack configured.

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+    <img width="150" src="https://i.ibb.co/QYzSw0S/maze.png"/>
+</p>
+
+# Maze Shooter
+
+Online multiplayer maze shooter game in development for a school project.
---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src', 'index.html'),
+        to: path.resolve(__dirname, 'dist')
+      },
+    ]),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
